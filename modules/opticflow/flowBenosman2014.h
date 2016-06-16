6e7425fec74b8b9be171a0fc58460f0ac46c23a3
baspijhor <baspijnackerh@gmail.com>
2016-06-16 14:28:38 +0200

    Optic flow event architecture and flow algorithm implemented

---
+++ modules/opticflow/flowBenosman2014.h
@@ -0,0 +1,24 @@
+/*
+ * flowComputation.h
+ *
+ *  Created on: May 30, 2016
+ *      Author: bas
+ */
+
+#ifndef FLOWBENOSMAN2014_H_
+#define FLOWBENOSMAN2014_H_
+
+#include "flowEvent.h"
+
+typedef struct  {
+	int64_t dtMin;
+	int64_t dtMax;
+	uint16_t dx;
+	double thr1;
+	double thr2;
+} FlowBenosman2014Params;
+
+void flowBenosman2014(FlowEvent e, FlowEventBuffer buffer,
+		FlowBenosman2014Params params);
+
+#endif /* FLOWBENOSMAN2014_H_ */
